4155e55c7c6b8e0df360b8473037c71a0e845c03
zushu <zumrudshukurlu@gmail.com>
2021-03-21 20:14:02 +0100

    express lab exercise 1

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -3,6 +3,8 @@ const app = new express();
 
 let loginDetails = [];
 
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 app.get("/",(req,res)=>{
     res.send("Welcome to the express server")
 })
@@ -20,6 +22,17 @@ app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
 })
 
+app.get("/fetch/:num",(req,res)=>{
+    let num = parseInt(req.params.num);
+    if (num > 0 && num <= 12)
+    {
+    res.send(num + "th month is " +months[num-1])
+    }
+    else 
+    {
+        res.send("invalid month number")
+    }
+})
 app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
